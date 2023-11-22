37c76ad3d5bc34a5c2781aee4058f7fc788e485d
qkrwlstjr04 <rodaeng132@gmail.com>
2023-11-22 17:23:13 +0900

    231122

---
+++ src/BOJ/B3_15894.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B3_15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		
+		System.out.println((n*2)+n+n);
+	}
+}
